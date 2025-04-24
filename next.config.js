ca7de2586135760dd100eb0796b7ed1b7b2bc989
Leo Byrne <leobyrne@dhcp-10-250-22-113.harvard.edu>
2025-04-23 20:09:10 -0400

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -10,6 +12,8 @@ const nextConfig = {
       },
     ],
   },
+  // Update this to your repository name
+  basePath: '/LFStartup',
 }
 
 module.exports = nextConfig 
